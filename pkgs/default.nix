099f06341a24b1e0339a375cc4226aa9f63487a5
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-17 00:53:15 +0100

    feat: Initial add of more pkgs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,16 @@
 { pkgs, inputs, ... }:
 with pkgs;
 {
-  email-gitsync = callPackage ./email-gitsync { inherit pkgs; };
+  email-gitsync = callPackage ./email-gitsync { };
   syncall = callPackage ./syncall { };
-  isync-exchange-patched = callPackage ./isync-exchange-patched { inherit pkgs; };
+  isync-exchange-patched = callPackage ./isync-exchange-patched { };
 } // lib.optionalAttrs (lib.hasSuffix "-linux" pkgs.system) {
   dwl = callPackage ./dwl { inherit inputs pkgs; };
-  wm-menu = callPackage ./wm-menu { inherit pkgs; };
+  is-net-metered = callPackage ./is-net-metered { };
+  wm-menu = callPackage ./wm-menu { };
+  wifi-qr = callPackage ./wifi-qr { };
+  wl-screen-share = callPackage ./wl-share-screen { };
+  wl-screen-share-stop = callPackage ./wl-share-screen-stop { };
+  buildbox = callPackage ./buildbox { };
+  buildstream = callPackage ./buildstream { };
 }
