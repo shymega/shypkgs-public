b546a96700ea15d7df0409c77022b2bda750c0fc
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-17 00:54:40 +0100

    feat: Add initial package for BuildStream & pyroaring
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ pkgs/buildstream/default.nix
@@ -0,0 +1,64 @@
+{ python3Packages
+, fetchPypi
+, lzip
+, patch
+, bubblewrap
+, fuse3
+}:
+let
+  inherit (python3Packages) buildPythonApplication buildPythonPackage;
+  # FIXME: Upstream `pyroraring` to Nixpkgs .
+  pyroaring = buildPythonPackage rec {
+    pname = "pyroaring";
+    version = "0.4.5";
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-gWyTuqXHKf+QYFb/7fcjyc3a8eqYioguDsUGKunqZzw=";
+    };
+    doCheck = false;
+    propagatedBuildInputs = with python3Packages; [
+      cython
+    ];
+    nativeBuildInputs = with python3Packages; [
+      pdm-pep517
+      setuptools-scm
+      pip
+    ] ++ [ pkgs.pdm ];
+  };
+in
+buildPythonApplication rec {
+  pname = "buildstream";
+  version = "2.3.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-lCdIKhwJWCqnXThYRma48S06bhp6NwByMGSX/nLBOTI=";
+  };
+
+  build-system = with python3Packages; [ setuptools ];
+
+  dependencies = with python3Packages; [
+    click
+    grpcio
+    jinja2
+    pluginbase
+    protobuf
+    psutil
+    ruamel-yaml
+    ruamel-yaml-clib
+    ujson
+    markupsafe
+  ] ++ [ pyroaring ];
+  propagatedBuildInputs = [
+    lzip
+    patch
+    bubblewrap
+    lzip
+    fuse3
+    python3Packages.cython
+  ];
+
+  nativeBuildInputs = with python3Packages; [ pdm-pep517 setuptools-scm ] ++ [ pkgs.pdm ];
+  doCheck = false;
+}
