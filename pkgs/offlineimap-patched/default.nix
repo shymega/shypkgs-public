841678afbda31c25848252add845c9a255c8acb9
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-24 00:07:37 +0100

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ pkgs/offlineimap-patched/default.nix
@@ -0,0 +1,20 @@
+{ pkgs }:
+let
+  inherit (pkgs) offlineimap fetchgit;
+in
+offlineimap.overrideAttrs (oldAttrs: {
+  pname = "offlineimap-patched";
+  inherit (oldAttrs) version;
+
+  src = fetchgit {
+    url = "https://github.com/shymega/offlineimap3";
+    rev = "9e5cdba6e75f6c69c41b8d9880bd7defc74708a4";
+    sha256 = "sha256-fap3ghJW7RariT/sPr+YcQtl5Q+7A31Uo+tXPtTQoFk=";
+  };
+
+  patches = [ ];
+
+  nativeBuildInputs = oldAttrs.nativeBuildInputs or [ ];
+
+  buildInputs = oldAttrs.buildInputs or [ ];
+})
