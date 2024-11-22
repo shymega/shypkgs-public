18a51a35a084167ef9af007eaa3937cc95fa1d19
Dom Rodriguez <shymega@shymega.org.uk>
2024-11-22 21:02:18 +0000

    feat: Bump to NixOS 24.11
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixfigs-helpers.url = "github:shymega/nixfigs-helpers";
   };
