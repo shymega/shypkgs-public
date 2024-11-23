f50a27fbf0a5e9685b0a90580df72a039ac72d1c
Dom Rodriguez <shymega@shymega.org.uk>
2024-11-23 17:20:57 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/dwl/default.nix
+++ pkgs/dwl/default.nix
@@ -10,32 +10,33 @@
     menu = pkgs.lib.getExe (pkgs.callPackage ../wm-menu {});
   },
   ...
-}: (pkgs.dwl.override {
-    enableXWayland = true;
-    # wayland = wayland-git;
-    # wayland-protocols = wayland-protocols-git;
-    # inherit wlroots;
-    # wayland-scanner = wayland-scanner-git;
-    configH = ./config.h;
-  })
-  .overrideAttrs
-  (oldAttrs: rec {
-    version = "0.7";
-    src = pkgs.fetchFromGitea {
-      domain = "codeberg.org";
-      owner = "dwl";
-      repo = "dwl";
-      rev = "v${version}";
-      hash = "sha256-7SoCITrbMrlfL4Z4hVyPpjB9RrrjLXHP9C5t1DVXBBA=";
-    };
-    buildInputs =
-      oldAttrs.buildInputs or [];
-    inherit patches;
-    postPatch = let
-      configFile = ./config.def.h;
-    in ''
-      cp ${configFile} config.def.h
-      substituteInPlace ./config.def.h --replace "@TERM@" "${cmd.terminal}"
-      substituteInPlace ./config.def.h --replace "@MENU@" "${cmd.menu}"
-    '';
-  })
+}:
+(pkgs.dwl.override {
+  enableXWayland = true;
+  # wayland = wayland-git;
+  # wayland-protocols = wayland-protocols-git;
+  # inherit wlroots;
+  # wayland-scanner = wayland-scanner-git;
+  configH = ./config.h;
+})
+.overrideAttrs
+(oldAttrs: rec {
+  version = "0.7";
+  src = pkgs.fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "dwl";
+    repo = "dwl";
+    rev = "v${version}";
+    hash = "sha256-7SoCITrbMrlfL4Z4hVyPpjB9RrrjLXHP9C5t1DVXBBA=";
+  };
+  buildInputs =
+    oldAttrs.buildInputs or [];
+  inherit patches;
+  postPatch = let
+    configFile = ./config.def.h;
+  in ''
+    cp ${configFile} config.def.h
+    substituteInPlace ./config.def.h --replace "@TERM@" "${cmd.terminal}"
+    substituteInPlace ./config.def.h --replace "@MENU@" "${cmd.menu}"
+  '';
+})
