cd144c16f8af98984ed5c95822a2ac356388f002
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-05 19:01:01 +0000

    WIP

--- pkgs/buildstream/buildstream-v2.nix
+++ pkgs/buildstream/buildstream-v2.nix
@@ -7,15 +7,15 @@
   fuse3,
   lib,
   pkgs,
+  buildbox,
 }: let
   inherit (python3Packages) buildPythonApplication buildPythonPackage;
-  # FIXME: Upstream `pyroaring` to Nixpkgs .
   pyroaring = buildPythonPackage rec {
     pname = "pyroaring";
     version = "0.4.5";
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-gWyTuqXHKf+QYFb/7fcjyc3a8eqYioguDsUGKunqZzw=";
+      hash = "sha256-gWyTuqXHKf+QYFb/7fcjyc3a8eqYioguDsUGKunqZzw=";
     };
     doCheck = false;
     propagatedBuildInputs = with python3Packages; [cython];
@@ -30,16 +30,15 @@
       platforms = lib.platforms.linux;
     };
   };
-  buildbox = pkgs.callPackage ../buildbox {};
 in
   buildPythonApplication rec {
     pname = "buildstream";
-    version = "2.3.0";
+    version = "2.4.0";
     pyproject = true;
 
     src = fetchPypi {
       inherit pname version;
-      hash = "sha256-lCdIKhwJWCqnXThYRma48S06bhp6NwByMGSX/nLBOTI=";
+      hash = "sha256-dj25ELy/79ouN8zRHvoSxX2XNdOgN5BNdbEiditIAro=";
     };
 
     build-system = with python3Packages; [setuptools];
@@ -47,24 +46,28 @@ in
     dependencies = with python3Packages;
       [
         click
+        dulwich
         grpcio
         jinja2
         markupsafe
+        packaging
         pluginbase
         protobuf
         psutil
+        requests
         ruamel-yaml
         ruamel-yaml-clib
+        tomlkit
         ujson
       ]
       ++ [pyroaring];
     propagatedBuildInputs = [
       bubblewrap
+      buildbox
       fuse3
       lzip
       patch
       python3Packages.cython
-      buildbox
     ];
 
     nativeBuildInputs = with python3Packages;
