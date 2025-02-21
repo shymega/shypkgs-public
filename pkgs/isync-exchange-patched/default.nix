87c3a4b753c3246c2e6aabb3f83e4ce861a41f95
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:44 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/isync-exchange-patched/default.nix
+++ pkgs/isync-exchange-patched/default.nix
@@ -1,49 +1,50 @@
 {
   pkgs,
   withCyrusSaslXoauth2 ? true,
-}:
-let
+}: let
   inherit (pkgs) isync fetchgit;
 in
-(isync.override { inherit withCyrusSaslXoauth2; }).overrideAttrs (oldAttrs: rec {
-  pname = "isync-exchange-patched";
-  version = "1.5.0";
+  (isync.override {inherit withCyrusSaslXoauth2;}).overrideAttrs (oldAttrs: rec {
+    pname = "isync-exchange-patched";
+    version = "1.5.0";
 
-  src = fetchgit {
-    url = "https://github.com/shymega/isync";
-    rev = "87796f0b813572c169dd0ac95b1efe377d833a8a";
-    sha256 = "sha256-+kYBW1UvlZz0lRRSfw0se+YcvllqisawsgjlSE04m3k=";
-  };
+    src = fetchgit {
+      url = "https://github.com/shymega/isync";
+      rev = "87796f0b813572c169dd0ac95b1efe377d833a8a";
+      sha256 = "sha256-+kYBW1UvlZz0lRRSfw0se+YcvllqisawsgjlSE04m3k=";
+    };
 
-  preConfigure = ''
-    touch ChangeLog
-    echo '${version}' > VERSION
-    ./autogen.sh
-  '';
+    preConfigure = ''
+      touch ChangeLog
+      echo '${version}' > VERSION
+      ./autogen.sh
+    '';
 
-  nativeBuildInputs =
-    oldAttrs.nativeBuildInputs or (
-      with pkgs;
-      [
-        libtool
-        pkgconfig
-      ]
-      ++ lib.optional withCyrusSaslXoauth2 pkgs.makeWrapper
-    )
-    ++ (with pkgs; [
-      autoconf
-      automake
-    ]);
+    nativeBuildInputs =
+      oldAttrs.nativeBuildInputs
+      or (
+        with pkgs;
+          [
+            libtool
+            pkgconfig
+          ]
+          ++ lib.optional withCyrusSaslXoauth2 pkgs.makeWrapper
+      )
+      ++ (with pkgs; [
+        autoconf
+        automake
+      ]);
 
-  buildInputs =
-    oldAttrs.buildInputs or (with pkgs; [
-      cyrus_sasl
-      db
-      openssl
-      zlib
-    ]);
+    buildInputs =
+      oldAttrs.buildInputs
+      or (with pkgs; [
+        cyrus_sasl
+        db
+        openssl
+        zlib
+      ]);
 
-  patches = [ ];
+    patches = [];
 
-  inherit (oldAttrs) postInstall;
-})
+    inherit (oldAttrs) postInstall;
+  })
