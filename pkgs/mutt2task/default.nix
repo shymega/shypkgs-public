6cf801b79168958df1a04c5084491f33cf82fce2
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:52 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/mutt2task/default.nix
+++ pkgs/mutt2task/default.nix
@@ -1,26 +1,27 @@
 {
   lib,
   fetchFromGitHub,
-  stdenv,
   python3Packages,
 }: let
   pname = "mutt2task";
   version = "unstable";
-in with python3Packages; buildPythonApplication rec {
-    inherit pname version;
-    pyproject = false;
-    src = fetchFromGitHub {
-      owner = "artur-shaik";
-      repo = "mutt2task";
-      rev = "34d120f3d3e8f2153fbd7847335d474959dfa425";
-      hash = "sha256-wP39LJmCt446XwOtGaE/CYXP8zBNnsGrXx1JjEytlLc=";
-    };
+in
+  with python3Packages;
+    buildPythonApplication rec {
+      inherit pname version;
+      pyproject = false;
+      src = fetchFromGitHub {
+        owner = "artur-shaik";
+        repo = "mutt2task";
+        rev = "34d120f3d3e8f2153fbd7847335d474959dfa425";
+        hash = "sha256-wP39LJmCt446XwOtGaE/CYXP8zBNnsGrXx1JjEytlLc=";
+      };
 
-    installPhase = "install -Dm755 ./mutt2task.py $out/bin/mutt2task";
+      installPhase = "install -Dm755 ./mutt2task.py $out/bin/mutt2task";
 
-    meta = with lib; {
-      maintainers = with maintainers; [shymega];
-      mainProgram = "mutt2task";
-      platforms = with platforms; unix;
-    };
-  }
+      meta = with lib; {
+        maintainers = with maintainers; [shymega];
+        mainProgram = "mutt2task";
+        platforms = with platforms; unix;
+      };
+    }
