79ad61d547a843b0af9cee896ea053096ed73a1f
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:41 +0000

    refactor: Tidy up `pkgs/default.nix`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,10 @@
-{ pkgs, system, inputs, ... }:
+{ pkgs, inputs, ... }:
 with pkgs;
-with pkgs.lib;
 {
   email-gitsync = callPackage ./email-gitsync { inherit pkgs; };
   syncall = callPackage ./syncall { };
-  isync-exchange-patched = callPackage ./isync-exchange-patched { };
-} // optionalAttrs (hasSuffix "-linux" pkgs.system) {
+  isync-exchange-patched = callPackage ./isync-exchange-patched { inherit pkgs; };
+} // lib.optionalAttrs (lib.hasSuffix "-linux" pkgs.system) {
   dwl = callPackage ./dwl { inherit inputs pkgs; };
   wm-menu = callPackage ./wm-menu { inherit pkgs; };
 }
