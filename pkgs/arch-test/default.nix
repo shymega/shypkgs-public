0213a4ec74d2251ff0840483b56df96cf8a7081c
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:52 +0000

    fix: Fix-up arguments for `arch-test`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/arch-test/default.nix
+++ pkgs/arch-test/default.nix
@@ -3,51 +3,39 @@
   stdenv,
   fetchFromGitHub,
   lib,
-  buildWin32 ? false,
-  buildWin64 ? false,
-  buildPPC64 ? false,
-  buildPPC32 ? false,
+  buildPPC64 ? true,
   buildArm64 ? true,
-  buildArm ? true,
-  buildM68k ? false,
-  buildMips ? false,
-  buildRiscv64 ? false,
-  buildRiscv32 ? false,
+  buildM68k ? true,
+  buildMips ? true,
+  buildRiscv64 ? true,
+  buildRiscv32 ? true,
   buildAmd64 ? true,
   ...
 }: let
-  pkgsCrossBase = pkgs.pkgsCross;
-
   pkgsCross = let
     inherit (lib) optionals;
+    inherit (pkgs) pkgsCross;
   in
-    optionals (pkgsCrossBase != null) []
-    ++ optionals buildPPC64 (with pkgsCrossBase.ppc64; [gcc binutils])
-    ++ optionals buildPPC32 (with pkgsCrossBase.ppc; [gcc binutils])
-    ++ optionals (buildArm || buildArm64) (with pkgsCrossBase.aarch64-multiplatform; [gcc binutils])
-    ++ optionals buildM68k (with pkgsCrossBase.m68k; [gcc binutils])
-    ++ optionals buildMips (with pkgsCrossBase.mips-linux-gnu; [gcc binutils])
-    ++ optionals buildRiscv64 (with pkgsCrossBase.riscv64; [gcc binutils])
-    ++ optionals buildRiscv32 (with pkgsCrossBase.riscv32; [gcc binutils])
-    ++ optionals buildAmd64 (with pkgs; [gcc binutils])
-    ++ optionals buildWin32 (with pkgsCrossBase.mingw32; [gcc binutils])
-    ++ optionals buildWin64 (with pkgsCrossBase.mingWW64; [gcc binutils]);
+    assert pkgsCross != null;
+    optionals buildPPC64 (with pkgsCross.ppc64; [gcc binutils])
+    ++ optionals buildArm64 (with pkgsCross.aarch64-multiplatform; [gcc binutils])
+    ++ optionals buildM68k (with pkgsCross.m68k; [gcc binutils])
+    ++ optionals buildMips (with pkgsCross.mips-linux-gnu; [gcc binutils])
+    ++ optionals buildRiscv64 (with pkgsCross.riscv64; [gcc binutils])
+    ++ optionals buildRiscv32 (with pkgsCross.riscv32; [gcc binutils])
+    ++ optionals buildAmd64 (with pkgs; [gcc binutils]);
 
   makeTargetArchs = let
     inherit (lib) optionalString concatStringsSep;
   in
     concatStringsSep " " [
       (optionalString buildPPC64 "ppc64")
-      (optionalString buildPPC32 "ppc")
-      (optionalString buildArm (concatStringsSep " " ["arm"]))
       (optionalString buildArm64 "arm64")
       (optionalString buildM68k "m68k")
       (optionalString buildMips "mips")
       (optionalString buildRiscv64 "riscv64")
       (optionalString buildRiscv32 "riscv32")
       (optionalString buildAmd64 "amd64")
-      (optionalString buildWin32 "win32")
-      (optionalString buildWin64 "win64")
     ];
 
   version = "0.21";
