7b894999c577b46f235861dacbb721749910252c
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:42 +0000

    fix: Fix the `amdgcn--` error with `dwl`, relating to Mesa
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/dwl/default.nix
+++ pkgs/dwl/default.nix
@@ -1,20 +1,31 @@
-{ pkgs, patches ? [ ], cmd ? { terminal = "wezterm"; menu = "${pkgs.dmenu}/bin/dmenu"; }, ... }:
+{ pkgs
+, patches ? [
+    ./dwl-patches/attachbottom.patch
+    ./dwl-patches/vanitygaps.patch
+    ./dwl-patches/focusdirection.patch
+  ]
+, cmd ? {
+    terminal = pkgs.lib.getExe pkgs.alacritty;
+    menu = pkgs.lib.getExe (pkgs.callPackage ../wm-menu { });
+  }
+, ...
+}:
 let
   wayland-src = pkgs.fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "wayland";
     repo = "wayland";
     rev = "1.23.1";
-    hash = "sha256-oK0Z8xO2ILuySGZS0m37ZF0MOyle2l8AXb0/6wai0/w=";
+    hash = "sha256-iNItt3XP70Y+bzsR2Hl+Lh4Cgh2LF1Ana9+zInLokkk=";
   };
 
   libdrm-git = pkgs.libdrm.overrideAttrs rec {
     pname = "libdrm";
     version = "2.4.123";
 
-    src = pkgs.fetchzip {
+    src = pkgs.fetchurl {
       url = "https://dri.freedesktop.org/${pname}/${pname}-${version}.tar.xz";
-      hash = "sha256-sSTpeg5jn30veyauz6aiLDTIui38TiswsX/gUgoipDY=";
+      hash = "sha256-ormFZ6FJp0sPUOkegl+cAxXYbnvpt0OU2uiymMqtt54=";
     };
   };
 
@@ -37,58 +48,52 @@ let
   wayland-protocols-git = (pkgs.wayland-protocols.override {
     wayland = wayland-git;
     wayland-scanner = wayland-scanner-git;
-  }).overrideAttrs (_: rec {
+  }).overrideAttrs rec {
     pname = "wayland-protocols";
     version = "1.37";
-
-    src = pkgs.fetchzip {
+    src = pkgs.fetchurl {
       url = "https://gitlab.freedesktop.org/wayland/${pname}/-/releases/${version}/downloads/${pname}-${version}.tar.xz";
-      hash = "sha256-BtVk4Ml13vobxd7cELonsPwsbe42k9FTQJdqsb/qOmk=";
+      hash = "sha256-pw6b6STy6GiOaCTc6vYYj6rNWuIY36yNCj0JdiEe8yY=";
     };
-  });
+  };
 
-  wlroots-0_18 = (pkgs.wlroots.override {
+  wlroots = (pkgs.wlroots_0_17.override {
     wayland = wayland-git;
     wayland-scanner = wayland-scanner-git;
     wayland-protocols = wayland-protocols-git;
     mesa = mesa-drm-git;
-  }).overrideAttrs (oldAttrs: rec {
-    version = "0.18.1";
-    src = pkgs.fetchFromGitLab {
-      domain = "gitlab.freedesktop.org";
-      owner = "wlroots";
-      repo = "wlroots";
-      rev = version;
-      hash = "sha256-GYlJXp6dFQgjJXc+T2Fs+1i7yuQjf5IM1+BWn+ivBOg=";
-    };
-    buildInputs = oldAttrs.buildInputs ++ (with pkgs; [
+  }).overrideAttrs (oldAttrs: {
+    patches = oldAttrs.patches ++ [
+      ./wlroots-patches/displaylink-custom.patch
+    ];
+    buildInputs = (oldAttrs.buildInputs or [ ]) ++ (with pkgs; [
       lcms2
-      hwdata
-      libdisplay-info
     ]);
+    inherit (oldAttrs) nativeBuildInputs version src;
+
   });
 in
 (pkgs.dwl.override {
   enableXWayland = true;
   wayland = wayland-git;
   wayland-protocols = wayland-protocols-git;
-  wlroots = wlroots-0_18;
+  inherit wlroots;
   wayland-scanner = wayland-scanner-git;
   conf = ./config.h;
 }).overrideAttrs
   (oldAttrs: rec {
-    version = "0.7";
+    version = "0.6";
     src = pkgs.fetchFromGitea {
       domain = "codeberg.org";
       owner = "dwl";
       repo = "dwl";
       rev = "v${version}";
-      hash = "sha256-7SoCITrbMrlfL4Z4hVyPpjB9RrrjLXHP9C5t1DVXBBA=";
+      hash = "sha256-fygUzEi4bgopesvHByfpatkLFYI98qozJOUBNM2t9Mg=";
     };
     buildInputs = (oldAttrs.buildInputs or [ ]) ++ (with pkgs; [
       libdrm
       fcft
-    ]);
+    ]) ++ [ wlroots ];
     inherit patches;
     postPatch =
       let
