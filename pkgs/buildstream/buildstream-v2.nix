6803de5dd3306e0494681faeaf35cb5a21396969
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:44 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/buildstream/buildstream-v2.nix
+++ pkgs/buildstream/buildstream-v2.nix
@@ -6,8 +6,7 @@
   bubblewrap,
   fuse3,
   lib,
-}:
-let
+}: let
   inherit (python3Packages) buildPythonApplication buildPythonPackage;
   # FIXME: Upstream `pyroaring` to Nixpkgs .
   pyroaring = buildPythonPackage rec {
@@ -18,65 +17,62 @@ let
       sha256 = "sha256-gWyTuqXHKf+QYFb/7fcjyc3a8eqYioguDsUGKunqZzw=";
     };
     doCheck = false;
-    propagatedBuildInputs = with python3Packages; [ cython ];
-    nativeBuildInputs =
-      with python3Packages;
+    propagatedBuildInputs = with python3Packages; [cython];
+    nativeBuildInputs = with python3Packages;
       [
         pdm-pep517
         setuptools-scm
         pip
       ]
-      ++ [ pkgs.pdm ];
+      ++ [pkgs.pdm];
     meta = {
       platforms = lib.platforms.linux;
     };
   };
 in
-buildPythonApplication rec {
-  pname = "buildstream";
-  version = "2.3.0";
-  pyproject = true;
+  buildPythonApplication rec {
+    pname = "buildstream";
+    version = "2.3.0";
+    pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-lCdIKhwJWCqnXThYRma48S06bhp6NwByMGSX/nLBOTI=";
-  };
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-lCdIKhwJWCqnXThYRma48S06bhp6NwByMGSX/nLBOTI=";
+    };
 
-  build-system = with python3Packages; [ setuptools ];
+    build-system = with python3Packages; [setuptools];
 
-  dependencies =
-    with python3Packages;
-    [
-      click
-      grpcio
-      jinja2
-      markupsafe
-      pluginbase
-      protobuf
-      psutil
-      ruamel-yaml
-      ruamel-yaml-clib
-      ujson
-    ]
-    ++ [ pyroaring ];
-  propagatedBuildInputs = [
-    bubblewrap
-    fuse3
-    lzip
-    patch
-    python3Packages.cython
-  ];
+    dependencies = with python3Packages;
+      [
+        click
+        grpcio
+        jinja2
+        markupsafe
+        pluginbase
+        protobuf
+        psutil
+        ruamel-yaml
+        ruamel-yaml-clib
+        ujson
+      ]
+      ++ [pyroaring];
+    propagatedBuildInputs = [
+      bubblewrap
+      fuse3
+      lzip
+      patch
+      python3Packages.cython
+    ];
 
-  nativeBuildInputs =
-    with python3Packages;
-    [
-      pdm-pep517
-      setuptools-scm
-    ]
-    ++ [ pkgs.pdm ];
-  doCheck = false;
+    nativeBuildInputs = with python3Packages;
+      [
+        pdm-pep517
+        setuptools-scm
+      ]
+      ++ [pkgs.pdm];
+    doCheck = false;
 
-  meta = {
-    platforms = lib.platforms.linux;
-  };
-}
+    meta = {
+      platforms = lib.platforms.linux;
+    };
+  }
