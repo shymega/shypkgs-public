d8e6e2028898bba55640181a4c8cf856f858605e
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:44 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/offlineimap-patched/default.nix
+++ pkgs/offlineimap-patched/default.nix
@@ -1,20 +1,19 @@
-{ pkgs }:
-let
+{pkgs}: let
   inherit (pkgs) offlineimap fetchgit;
 in
-offlineimap.overrideAttrs (oldAttrs: {
-  pname = "offlineimap-patched";
-  inherit (oldAttrs) version;
+  offlineimap.overrideAttrs (oldAttrs: {
+    pname = "offlineimap-patched";
+    inherit (oldAttrs) version;
 
-  src = fetchgit {
-    url = "https://github.com/shymega/offlineimap3";
-    rev = "9e5cdba6e75f6c69c41b8d9880bd7defc74708a4";
-    sha256 = "sha256-fap3ghJW7RariT/sPr+YcQtl5Q+7A31Uo+tXPtTQoFk=";
-  };
+    src = fetchgit {
+      url = "https://github.com/shymega/offlineimap3";
+      rev = "9e5cdba6e75f6c69c41b8d9880bd7defc74708a4";
+      sha256 = "sha256-fap3ghJW7RariT/sPr+YcQtl5Q+7A31Uo+tXPtTQoFk=";
+    };
 
-  patches = [ ];
+    patches = [];
 
-  nativeBuildInputs = oldAttrs.nativeBuildInputs or [ ];
+    nativeBuildInputs = oldAttrs.nativeBuildInputs or [];
 
-  buildInputs = oldAttrs.buildInputs or [ ];
-})
+    buildInputs = oldAttrs.buildInputs or [];
+  })
