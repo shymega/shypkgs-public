744b66e6b31556b4f301944db9b35e357e5d1733
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:47 +0000

    feat: Add `bst-to-lorry` package
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ pkgs/bst-to-lorry/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  pkgs,
+  fetchFromGitLab,
+  python3Packages,
+}: let
+  inherit (python3Packages) buildPythonApplication;
+  buildstream2 = pkgs.callPackage ../buildstream/buildstream-v2.nix { };
+in
+  buildPythonApplication rec {
+    pname = "bst-to-lorry";
+    version = "1.0.0";
+    pyproject = true;
+
+    src = fetchFromGitLab {
+      owner = "CodethinkLabs/lorry";
+      repo = "bst-to-lorry";
+      rev = "main";
+      hash = "sha256-6k+jaiWkamOy6x/9e7WbwhiBlH06sulA6S9wZFsqMco=";
+    };
+
+    build-system = with python3Packages; [setuptools setuptools-scm];
+
+    propagatedBuildInputs =
+      [
+        buildstream2
+        python3Packages.pyyaml
+      ];
+
+    doCheck = false;
+
+    meta = {
+      platforms = lib.platforms.linux;
+      mainProgram = "bst-to-lorry";
+    };
+  }
