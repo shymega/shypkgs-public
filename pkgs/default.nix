c9c3f5a6679cbb21c9bf58400494f568d782ba9e
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:46 +0000

    fix: Disable `wifi-qr` package
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -13,7 +13,7 @@ with pkgs;
     dwl = callPackage ./dwl {inherit inputs pkgs;};
     is-net-metered = callPackage ./is-net-metered {};
     wm-menu = callPackage ./wm-menu {};
-    wifi-qr = callPackage ./wifi-qr {};
+    # wifi-qr = callPackage ./wifi-qr {};
     wl-screen-share = callPackage ./wl-share-screen {};
     wl-screen-share-stop = callPackage ./wl-share-screen-stop {};
     buildbox = callPackage ./buildbox {};
