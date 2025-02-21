14793603ddb76cc502abbb9d7c9ac86817bbbc93
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:48 +0000

    fix: Re-add `pytest-runner` to buildstream1
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/buildstream/buildstream-v1.nix
+++ pkgs/buildstream/buildstream-v1.nix
@@ -5,9 +5,21 @@
   fuse2,
   lzip,
   patch,
-  python3Packages,
+  python310Packages,
 }: let
-  inherit (python3Packages) buildPythonApplication;
+  inherit (python310Packages) buildPythonApplication buildPythonPackage;
+  pytest-runner = buildPythonPackage rec {
+    pname = "pytest-runner";
+    version = "6.0.1";
+    pyproject = true;
+
+    src = fetchPypi {
+      inherit pname version;
+      hash = "sha256-cNRzlYWnAI83v0kzwBP9sye4h4paafy7MxbIiILw9Js=";
+    };
+
+    build-system = with python310Packages; [setuptools setuptools-scm];
+  };
 in
   buildPythonApplication rec {
     pname = "BuildStream";
@@ -19,26 +31,28 @@ in
       hash = "sha256-rEYGwCbpbBBMeSpRdsETSHGivDxQVm/PSFW5DmEZtGA=";
     };
 
-    build-system = with python3Packages; [setuptools];
+    build-system = with python310Packages; [setuptools];
 
-    dependencies = with python3Packages; [
-      click
-      fusepy
-      grpcio
-      jinja2
-      markupsafe
-      pluginbase
-      protobuf
-      psutil
-      pytest-runner
-      ruamel-yaml
-      ruamel-yaml-clib
-      six
-      ujson
-    ];
+    dependencies = with python310Packages;
+      [
+        click
+        fusepy
+        grpcio
+        jinja2
+        markupsafe
+        pluginbase
+        protobuf
+        psutil
+        ruamel-yaml
+        ruamel-yaml-clib
+        six
+        ujson
+        versioneer
+      ]
+      ++ [pytest-runner];
 
     nativeBuildInputs =
-      (with python3Packages; [
+      (with python310Packages; [
         pdm-pep517
         setuptools-scm
       ])
@@ -49,7 +63,7 @@ in
         lzip
         patch
       ]
-      ++ (with python3Packages; [
+      ++ (with python310Packages; [
         cython
         fusepy
       ]);
