ac67a87fccf592ae7ab458bd8408d721b66868cd
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:42 +0000

    refactor: Tidy up and refactor `wm-menu`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/wm-menu/default.nix
+++ pkgs/wm-menu/default.nix
@@ -1,9 +1,20 @@
 { pkgs
 }:
+let
+  inherit (pkgs.lib) getExe;
+in
 pkgs.writeShellApplication {
   name = "wm-menu";
   text = ''
     #!/bin/sh
-    exec ${pkgs.rofi} -combi-modi "window,drun,ssh,run" -show combi -modi combi -lines 20 -hide-scrollbar
+    if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
+      echo "INFO: Launching menu with Wayland support.."
+      exec ${getExe pkgs.rofi-wayland} -combi-modi "window,drun,ssh,run" -show combi -modi combi -lines 20 -hide-scrollbar
+    else
+      echo "INFO: Launching menu with X11 support.."
+       exec ${getExe pkgs.rofi} -combi-modi "window,drun,ssh,run" -show combi -modi combi -lines 20 -hide-scrollbar
+    fi
+
+    exit
   '';
 }
