5f07bcf2e8ad99303238e97872dbe73b0cd812aa
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:44 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/buildbox/default.nix
+++ pkgs/buildbox/default.nix
@@ -18,49 +18,48 @@
   libuuid,
   tomlplusplus,
   fuse3,
-}:
-let
+}: let
   pname = "buildbox";
   version = "1.2.25";
 in
-stdenv.mkDerivation {
-  inherit pname version;
+  stdenv.mkDerivation {
+    inherit pname version;
 
-  buildInputs = [
-    cmake
-    pkg-config
-    ninja
-    bubblewrap
-  ];
-  nativeBuildInputs = [
-    grpc
-    gbenchmark
-    gtest
-    glog
-    protobuf
-    nlohmann_json
-    zlib
-    openssl
-    libuuid
-    tomlplusplus
-    fuse3
-    makeWrapper
-  ];
+    buildInputs = [
+      cmake
+      pkg-config
+      ninja
+      bubblewrap
+    ];
+    nativeBuildInputs = [
+      grpc
+      gbenchmark
+      gtest
+      glog
+      protobuf
+      nlohmann_json
+      zlib
+      openssl
+      libuuid
+      tomlplusplus
+      fuse3
+      makeWrapper
+    ];
 
-  src = fetchFromGitLab {
-    domain = "gitlab.com";
-    owner = "BuildGrid";
-    repo = "${pname}/${pname}";
-    rev = "${version}";
-    sha256 = "sha256-8umP9tUnSiB+ujlaMDrkwpU9269h/MGZZ2MsZS/c/Xs=";
-  };
+    src = fetchFromGitLab {
+      domain = "gitlab.com";
+      owner = "BuildGrid";
+      repo = "${pname}/${pname}";
+      rev = "${version}";
+      sha256 = "sha256-8umP9tUnSiB+ujlaMDrkwpU9269h/MGZZ2MsZS/c/Xs=";
+    };
 
-  postFixup = ''
-    wrapProgram $out/bin/buildbox-run-bubblewrap --prefix PATH : ${lib.makeBinPath [ bubblewrap ]}
-    ln -s buildbox-run-bubblewrap $out/bin/buildbox-run
-  '';
+    postFixup = ''
+      wrapProgram $out/bin/buildbox-run-bubblewrap --prefix PATH : ${lib.makeBinPath [bubblewrap]}
+      ln -s buildbox-run-bubblewrap $out/bin/buildbox-run
+    '';
 
-  meta = {
-    platforms = lib.platforms.linux;
-  };
-}
+    meta = {
+      platforms = lib.platforms.linux;
+    };
+  }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,19 +1,23 @@
-{ pkgs, inputs, ... }:
-with pkgs;
 {
-  email-gitsync = callPackage ./email-gitsync { };
-  syncall = callPackage ./syncall { };
-  isync-exchange-patched = callPackage ./isync-exchange-patched { };
-  offlineimap-patched = callPackage ./offlineimap-patched { };
-}
-// lib.optionalAttrs (lib.hasSuffix "-linux" pkgs.system) {
-  dwl = callPackage ./dwl { inherit inputs pkgs; };
-  is-net-metered = callPackage ./is-net-metered { };
-  wm-menu = callPackage ./wm-menu { };
-  wifi-qr = callPackage ./wifi-qr { };
-  wl-screen-share = callPackage ./wl-share-screen { };
-  wl-screen-share-stop = callPackage ./wl-share-screen-stop { };
-  buildbox = callPackage ./buildbox { };
-  buildstream1 = callPackage ./buildstream/buildstream-v1.nix { };
-  buildstream2 = callPackage ./buildstream/buildstream-v2.nix { };
-}
+  pkgs,
+  inputs,
+  ...
+}:
+with pkgs;
+  {
+    email-gitsync = callPackage ./email-gitsync {};
+    syncall = callPackage ./syncall {};
+    isync-exchange-patched = callPackage ./isync-exchange-patched {};
+    offlineimap-patched = callPackage ./offlineimap-patched {};
+  }
+  // lib.optionalAttrs (lib.hasSuffix "-linux" pkgs.system) {
+    dwl = callPackage ./dwl {inherit inputs pkgs;};
+    is-net-metered = callPackage ./is-net-metered {};
+    wm-menu = callPackage ./wm-menu {};
+    wifi-qr = callPackage ./wifi-qr {};
+    wl-screen-share = callPackage ./wl-share-screen {};
+    wl-screen-share-stop = callPackage ./wl-share-screen-stop {};
+    buildbox = callPackage ./buildbox {};
+    buildstream1 = callPackage ./buildstream/buildstream-v1.nix {};
+    buildstream2 = callPackage ./buildstream/buildstream-v2.nix {};
+  }
