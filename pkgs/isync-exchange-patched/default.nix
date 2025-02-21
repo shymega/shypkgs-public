c41beb3973a05a96383fa9aa444d496e01cbf15d
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:20:42 +0000

    refactor: Tidy up `isync-exchange-patched` package
    Instead of duplicating upstream Nixpkgs's derivation for `isync`,
    instead, we use `override` & `overrideAttrs`, as well as `inherit`.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/isync-exchange-patched/default.nix
+++ pkgs/isync-exchange-patched/default.nix
@@ -1,56 +1,35 @@
-{ lib
-, stdenv
-, fetchgit
-, pkg-config
-, autoconf
-, automake
-, perl
-, openssl
-, db
-, cyrus_sasl
-, zlib
-, withCyrusSaslXoauth2 ? false
-, cyrus-sasl-xoauth2
-, makeWrapper
+{ pkgs
+, withCyrusSaslXoauth2 ? true
 }:
-
-stdenv.mkDerivation rec {
+let
+  inherit (pkgs) isync fetchgit;
+in
+(isync.override ({
+  inherit withCyrusSaslXoauth2;
+})).overrideAttrs (oldAttrs: rec {
   pname = "isync-exchange-patched";
   version = "1.5.0";
 
   src = fetchgit {
     url = "https://github.com/shymega/isync";
-    rev = "0af93316ff2d62472e3faa23420180910b32d858";
-    sha256 = "sha256-yqe+qiZLletTuVD9pQKDX57wCBh2LoOr2QBJFBNnhNE=";
+    rev = "87796f0b813572c169dd0ac95b1efe377d833a8a";
+    sha256 = "sha256-+kYBW1UvlZz0lRRSfw0se+YcvllqisawsgjlSE04m3k=";
   };
-
-  nativeBuildInputs = [ autoconf automake perl pkg-config ]
-    ++ lib.optionals withCyrusSaslXoauth2 [ makeWrapper ];
-  buildInputs = [ cyrus_sasl db openssl zlib ];
 
   preConfigure = ''
     touch ChangeLog
+    echo '${version}' > VERSION
     ./autogen.sh
   '';
 
-  postInstall = lib.optionalString withCyrusSaslXoauth2 ''
-    wrapProgram "$out/bin/mbsync" \
-        --prefix SASL_PATH : "${lib.makeSearchPath "lib/sasl2" [ cyrus-sasl-xoauth2 cyrus_sasl.out ]}"
-  '';
+  nativeBuildInputs = (oldAttrs.nativeBuildInputs or (with pkgs; [
+    libtool
+    pkgconfig
+  ] ++ lib.optional withCyrusSaslXoauth2 pkgs.makeWrapper) ++ (with pkgs; [ autoconf automake ]));
 
-  meta = with lib; {
-    homepage = "http://isync.sourceforge.net/";
-    # https://sourceforge.net/projects/isync/
-    changelog = "https://sourceforge.net/p/isync/isync/ci/v${version}/tree/NEWS";
-    description = "Free IMAP and MailDir mailbox synchronizer";
-    longDescription = ''
-      mbsync (formerly isync) is a command line application which synchronizes
-      mailboxes. Currently Maildir and IMAP4 mailboxes are supported. New
-      messages, message deletions and flag changes can be propagated both ways.
-    '';
-    license = licenses.gpl2Plus;
-    platforms = platforms.unix;
-    maintainers = with maintainers; [ primeos ];
-    mainProgram = "mbsync";
-  };
-}
+  buildInputs = (oldAttrs.buildInputs or (with pkgs; [ cyrus_sasl db openssl zlib ]));
+
+  patches = [ ];
+
+  inherit (oldAttrs) postInstall;
+})
