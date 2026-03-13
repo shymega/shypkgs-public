ad829f1809a9e8c0d5b354141c1ad2b729ef5a64
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-13 18:42:47 +0000

    fix: Disable `arch-test`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,11 +10,11 @@
   mutt2task = pkgs.callPackage ./mutt2task {};
 }
 // lib.optionalAttrs pkgs.stdenv.isLinux {
+  # arch-test = pkgs.callPackage ./arch-test {};
+  bst-to-lorry = pkgs.callPackage ./bst-to-lorry {};
   dwl = pkgs.callPackage ./dwl {inherit inputs pkgs;};
   is-net-metered = pkgs.callPackage ./is-net-metered {};
-  wm-menu = pkgs.callPackage ./wm-menu {};
   wl-screen-share = pkgs.callPackage ./wl-share-screen {};
   wl-screen-share-stop = pkgs.callPackage ./wl-share-screen-stop {};
-  bst-to-lorry = pkgs.callPackage ./bst-to-lorry {};
-  arch-test = pkgs.callPackage ./arch-test {};
+  wm-menu = pkgs.callPackage ./wm-menu {};
 }
