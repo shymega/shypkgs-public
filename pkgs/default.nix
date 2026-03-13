e034b8b59b2e9b62b3297fcd4a3b7463457a59cc
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-13 18:18:15 +0000

    fix: Fix error parsing Flake
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,20 +1,20 @@
 {
-  pkgs,
+  pkgs ? inputs.nixpkgs.legacyPackages.x86_64-linux,
   inputs,
   lib ? inputs.nixpkgs.lib,
   ...
 }:
-  {
-    email-gitsync = pkgs.callPackage ./email-gitsync {};
-    git-wip = pkgs.callPackage ./git-wip {};
-    mutt2task = pkgs.callPackage ./mutt2task {};
-  }
-  // lib.optionalAttrs pkgs.stdenv.isLinux {
-    dwl = pkgs.callPackage ./dwl {inherit inputs pkgs;};
-    is-net-metered = pkgs.callPackage ./is-net-metered {};
-    wm-menu = pkgs.callPackage ./wm-menu {};
-    wl-screen-share = pkgs.callPackage ./wl-share-screen {};
-    wl-screen-share-stop = pkgs.callPackage ./wl-share-screen-stop {};
-    bst-to-lorry = pkgs.callPackage ./bst-to-lorry {};
-    arch-test = pkgs.callPackage ./arch-test {};
-  }
+{
+  email-gitsync = pkgs.callPackage ./email-gitsync {};
+  git-wip = pkgs.callPackage ./git-wip {};
+  mutt2task = pkgs.callPackage ./mutt2task {};
+}
+// lib.optionalAttrs pkgs.stdenv.isLinux {
+  dwl = pkgs.callPackage ./dwl {inherit inputs pkgs;};
+  is-net-metered = pkgs.callPackage ./is-net-metered {};
+  wm-menu = pkgs.callPackage ./wm-menu {};
+  wl-screen-share = pkgs.callPackage ./wl-share-screen {};
+  wl-screen-share-stop = pkgs.callPackage ./wl-share-screen-stop {};
+  bst-to-lorry = pkgs.callPackage ./bst-to-lorry {};
+  arch-test = pkgs.callPackage ./arch-test {};
+}
