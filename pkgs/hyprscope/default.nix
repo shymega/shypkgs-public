8d9c880dde30cfdf403f109ec1a1a733f4e672a2
Dom Rodriguez <shymega@shymega.org.uk>
2026-04-07 22:44:17 +0100

    feat: Add hyprscope package
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ pkgs/hyprscope/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  fetchFromGitHub,
+  stdenv,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "hyprscope";
+  version = "unstable";
+
+  src = fetchFromGitHub {
+    owner = "bajankristof";
+    repo = finalAttrs.pname;
+    rev = "5dcb36788c487a6947b8209f8743c563629c118e";
+    hash = "sha256-9p4hiX4595QeJ6gzHFjUbtzLqkb8gyydvAS4OP1yaWs=";
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    install -Dm755 -t $out/bin hyprscope
+  '';
+
+  meta = with lib; {
+    maintainers = with maintainers; [shymega];
+    mainProgram = "hyprscope";
+    platforms = with platforms; unix;
+  };
+})
