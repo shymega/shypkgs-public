977311c26387c64cc7972436ba832b63d080c7f2
Dom Rodriguez <shymega@shymega.org.uk>
2026-04-07 22:43:35 +0100

    feat: Add hyprland-cursor-lock package
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ pkgs/hyprland-cursor-lock/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  fetchFromGitLab,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "hyprland-cursor-lock";
+  version = "unstable";
+
+  src = fetchFromGitLab {
+    domain = "gitlab.com";
+    owner = "oriionn";
+    repo = finalAttrs.pname;
+    rev = "69b3eefd6d46cb3fa33dcf077551bf25c3a54ff0";
+    hash = "sha256-TEEQK7cwCFgPx+KDD07bwL9OCtvczkB4lA93RoQc634=";
+  };
+
+  cargoHash = "sha256-qQJOi2AZfKAl+JCDUQniSh0c1aH9ofM8Jrg54bqIvQo=";
+})
