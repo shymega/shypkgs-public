82c51170c843698e86588f04541e5c421e5e23f6
Dom Rodriguez <shymega@shymega.org.uk>
2026-04-12 20:56:52 +0100

    refactor: Refactor package derivations

--- pkgs/wm-menu/default.nix
+++ pkgs/wm-menu/default.nix
@@ -1,13 +1,22 @@
-{pkgs}: let
-  inherit (pkgs.lib) getExe;
+{
+  writeShellApplication,
+  rofi,
+  lib,
+}: let
+  inherit (lib) getExe;
 in
-  pkgs.writeShellApplication {
+  writeShellApplication rec {
     name = "wm-menu";
     text = ''
       #!/bin/sh
       echo "INFO: Launching menu with X11 support.."
-       exec ${getExe pkgs.rofi} -combi-modi "window,drun,ssh,run" -show combi -modi combi -lines 20 -hide-scrollbar
+       exec ${getExe rofi} -combi-modi "window,drun,ssh,run" -show combi -modi combi -lines 20 -hide-scrollbar
 
       exit
     '';
+
+    meta = {
+      mainProgram = name;
+      inherit (rofi.meta) platforms;
+    };
   }
